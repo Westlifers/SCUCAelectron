4fb212cf8e706ae13fe450ceed914bb1e7506c78
Westlifers <2585687459@qq.com>
2023-03-01 13:16:19 +0800

    fix bug: 666 and such events were displayed as 5-time events

--- src/components/competitionDetail/DataTable.vue
+++ src/components/competitionDetail/DataTable.vue
@@ -4,8 +4,8 @@
     <el-table-column prop="time_1" label="第一次" :formatter="formatter" width="120"/>
     <el-table-column prop="time_2" label="第二次" :formatter="formatter" width="120"/>
     <el-table-column prop="time_3" label="第三次" :formatter="formatter" width="120"/>
-    <el-table-column prop="time_4" label="第四次" :formatter="formatter" width="120"/>
-    <el-table-column prop="time_5" label="第五次" :formatter="formatter" width="120"/>
+    <el-table-column prop="time_4" label="第四次" :formatter="formatter" v-if="maxScrambleCount===5" width="120"/>
+    <el-table-column prop="time_5" label="第五次" :formatter="formatter" v-if="maxScrambleCount===5" width="120"/>
     <el-table-column prop="avg" label="平均" :formatter="formatter" width="120" :sortable="true" :sort-method="sort_avg_count_in_zero"/>
     <el-table-column prop="best" label="最佳" :formatter="formatter" width="120" :sortable="true" :sort-method="sort_best_count_in_zero"/>
   </el-table>
@@ -15,12 +15,22 @@
 import {TableColumnCtx} from "element-plus";
 import {Result} from "@/types";
 import {time_convert} from "@/utils";
+import {computed} from "vue";
 
 
 const formatter = (row: Result, column: TableColumnCtx<Result>) => {
   const val = row[column.property]
   return val>0?time_convert(val):'DNF'
 }
+
+const maxScrambleCount = computed(() => {
+  const specs: string[] = ['333bld', '444bld', '555bld', '666', '777']
+  if (specs.indexOf(props.tableData[0].event) > -1) {
+    return 3
+  } else {
+    return 5
+  }
+})
 
 const sort_avg_count_in_zero = (obj_a, obj_b) => {
   let a = obj_a.avg
@@ -46,7 +56,7 @@ const sort_best_count_in_zero = (obj_a, obj_b) => {
   return a - b
 }
 
-defineProps<{
+const props = defineProps<{
   tableData: Result[]
 }>()
 </script>
